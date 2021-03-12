669065f7d358bf06b93e968e01fd51faaaf0ee8d
Rodrigo Gama <ro.gama91@gmail.com>
2021-03-12 12:20:54 -0300

    feat: add temporary splash scren

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,73 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React from 'react';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
-export default function Home() {
+const Home = () => {
+  React.useLayoutEffect(() => {
+    if (window) {
+      document
+        .querySelector('html')
+        .setAttribute('style', `--vh: ${0.01 * window.innerHeight}px`);
+    }
+  }, []);
+
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>Under construction</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <main className={styles.content}>
+        <div className={styles.container}>
+          <div className={styles.splashWrapper}>
+            <section className={styles.splashHero}>
+              <div className={styles.textWrapper}>
+                <div className={styles.textContainer}>
+                  <div className={styles.textDescription}>
+                    <strong className={styles.textSplit}>
+                      <div className={styles.textLine}>
+                        {`Hi, my website is mutating and it will be ready very
+                        soon, `}
+                      </div>
+                      <div className={styles.textLine}>
+                        {`until then you can contact me on `}
+                        <a
+                          href="https://www.linkedin.com/in/rodrigo-gama-baptista/"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          Linkedin
+                        </a>
+                        {` or `}
+                        <a
+                          href="https://www.instagram.com/orodrigogama/"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          Instagram
+                        </a>
+                      </div>
+                      <div className={styles.textLine}>
+                        to keep up to date with my latest works.
+                      </div>
+                    </strong>
+                  </div>
+                </div>
+                <h1 className={styles.titleContainer}>
+                  <div className={styles.titleLine}>
+                    <div className={styles.word}>Good</div>
+                    <strong>things</strong>
+                    <div className={styles.word}>take</div>
+                    <strong>time.</strong>
+                  </div>
+                </h1>
+              </div>
+            </section>
+          </div>
         </div>
       </main>
+    </>
+  );
+};
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
-}
+export default Home;
